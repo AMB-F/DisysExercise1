6dc16f160b2ec5ebc84abd7406dd0cce797ee97e
Andreas Wachs Hjalager <ahja@itu.dk>
2021-10-05 11:34:07 +0200

    Add all rpc calls to client

--- gRPC/client/client.go
+++ gRPC/client/client.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	grpc "google.golang.org/grpc"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 func main() {
@@ -25,14 +26,49 @@ func main() {
 	defer cancel()
 
 	var id int32 = 1
-	r, err := c.GetCourse(ctx, &pb.CourseRequest{
+
+	// NewCourse
+	newCourseReply, err := c.NewCourse(ctx, &emptypb.Empty{})
+
+	handleError(err, "new course")
+	log.Printf("NewCourse: %s", newCourseReply)
+
+	// GetCourse call
+	getCourseReply, err := c.GetCourse(ctx, &pb.CourseRequest{
 		Action:   pb.CourseRequest_GET,
 		CourseId: &id,
 	})
 
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
+	handleError(err, "edit course")
+	log.Printf("GetCourse: %s", getCourseReply)
 
-	log.Printf("Greeting: %s", r)
+	// DeleteCourse call
+	deleteCourseReply, err := c.DeleteCourse(ctx, &pb.CourseRequest{
+		Action:   pb.CourseRequest_DELETE,
+		CourseId: &id,
+	})
+
+	handleError(err, "delete course")
+	log.Printf("DeleteCourse: %s", deleteCourseReply)
+
+	newCourseState := pb.CourseDTO{
+		Id:               id + 1,
+		Name:             "Distributed Systems",
+		ExpectedWorkload: 1000000,
+		Rating:           5.0,
+	}
+	editCourseReply, err := c.EditCourse(ctx, &pb.CourseRequest{
+		Action:         pb.CourseRequest_PUT,
+		CourseId:       &id,
+		NewCourseState: &newCourseState,
+	})
+
+	handleError(err, "edit course")
+	log.Printf("EditCourse: %s", editCourseReply)
+}
+
+func handleError(err error, annotation string) {
+	if err != nil {
+		log.Fatalf("could not get  %s: %v", annotation, err)
+	}
 }
